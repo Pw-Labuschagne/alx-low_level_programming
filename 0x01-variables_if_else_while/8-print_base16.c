eca51bd017d202b8792dd121df60163d2447ad98
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-05 04:50:21 -0700

    0x00-hello_world

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - again
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+char c;
+for (i = '0'; i <= '9' ; i++)
+{
+putchar(i);
+}
+for (c = 'a' ; c <= 'f' ; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}

e1bf53dce80dc85e6a82dee7a55892b2fd2f2b64
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-05 03:57:42 -0700

    0x00-hello_world

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/** 
+ * main - yet again
+ * Return 0
+ */
+
+int main(void)
+{
+char = a;
+
+for (a = 'a' , a <= 'z' , c++)
+{
+putchar(c);
+}
+for (a = 'A' , a <= 'Z' , c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}

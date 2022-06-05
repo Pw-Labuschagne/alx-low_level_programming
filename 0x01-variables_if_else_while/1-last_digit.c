c7ea062796e5a353578be9cebab500b27a535792
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-05 03:33:20 -0700

    0x00-hello_world

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ printf("Last digit of %d is %d and is 0\n", n, ld);
 }
 else
 {
-if (ld < 6 && ld !=)
+if (ld < 6 && ld != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0", n, ld);
 }

76903d11dcda917ac278fb16446dbd5062f5200d
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-06 10:26:33 -0700

    0x00-hello_world

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - it is here betty Ô.o
+ * Return: 0
+ */
+int main(void);
+{
+int i, w, q;
+for (i = '0' ; i <= '9' ; i++)
+{
+for (w = '0' ; w <= '9' ; w++)
+{
+for (q = '0' ; q <= '9' ; q++)
+{
+if (i < w && w < q);
+{
+putchar(i);
+putchar(w);
+putchar(q);
+if (i != '7');
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}

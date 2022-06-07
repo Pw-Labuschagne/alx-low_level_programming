0dede1c77008fd8b569ba9c0cb34a97ab4246e3d
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-07 15:31:59 -0700

    0x00-hello_world

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,23 +7,23 @@
 void times_table(void)
 {
 int a, b, c;
-for (a = 0; a <= 9; a++)
+for (a = 0; a < 10; a++)
 {
-for (b = 0; b <= 9; b++)
+for (b = 0; b < 10; b++)
 {
 c = a * b;
 if (b == 0)
 {
 _putchar(c + '0');
 }
-if (c <= 9 && b != 0)
+if (c <10 && b != 0)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(c + '0');
 }
-if (c >= 9)
+if (c > 10)
 {
 _putchar(',');
 _putchar(' ');

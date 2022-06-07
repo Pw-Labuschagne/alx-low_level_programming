4d58657a70bbaca3b4aa919a7eac2d5ceefc186b
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-07 16:07:06 -0700

    0x00-hello_world

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,35 +9,32 @@ void print_to_98(int n)
 {
 int z, x;
 if (n <= 98)
-{
-for (z = n; z <= 98; z++)
-{
-if (z != 98)
-{
-_putchar(z);
-_putchar(',');
-_putchar(' ');
-}
-else if (z == 98)
-{
-_putchar(z);
-_putchar('\n');
-}
-}
-}
+	{
+		for (z = n; z <= 98; z++)
+			{
+				if (z != 98)
+					{
+						_putchar(z);
+						_putchar(',');
+						_putchar(' ');
+				else if (z == 98)
+						_putchar(z);
+						_putchar('\n');
+					}
+			}
+	}
 else if (n >= 98)
-{
-for (x = n; x >= 98; x--)
-{
-if (x != 98)
-{
-_putchar(x);
-_putchar(',');
-_putchar(' ');
-}
-else if (x == 98)
-{
-_putchar(x);
-_putchar('\n');
-}
+	{
+		for (x = n; x >= 98; x--)
+			{
+				if (x != 98)
+					{
+						_putchar(x);
+						_putchar(',');
+						_putchar(' ');			
+				else if (x == 98)					
+						_putchar(x);
+						_putchar('\n');
+					}
+	}
 }

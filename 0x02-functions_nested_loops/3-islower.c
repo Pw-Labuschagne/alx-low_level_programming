46bdbb2f63ad62b520e29b2cd4c51cc65a60c173
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-07 13:15:28 -0700

    0x00-hello_world

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * _islower(int c);
+ * Return: 1
+ */
+int _islower(int c);
+{
+if (islower(c))
+return (1);
+else
+{
+return (0);
+}
+}

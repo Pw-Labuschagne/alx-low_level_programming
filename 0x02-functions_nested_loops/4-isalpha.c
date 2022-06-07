8670ca78bf9ac1122c8ffe2611e4923d2cfb674d
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-07 13:36:23 -0700

    0x00-hello_world

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio>
+#include "main.h"
+/**
+ * int _isalpha.c - is it alpha
+ * @c: parameter
+ * Return 1 or 0
+ */
+int _isalpha.c(int c)
+if ((c >= 'a' && c >='A') && (c <= 'z' && c <= 'A'))
+{
+return (1);
+else
+return (0);
+}

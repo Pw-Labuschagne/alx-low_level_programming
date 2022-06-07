f72064b4f72e6f1fbdb25f343142caba8f59afe6
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-07 14:19:29 -0700

    0x00-hello_world

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - well the name
+ * @x: parameter
+ * Return: r
+ */
+int print_last_digit(int x)
+{
+int ld;
+ld = x % 10;
+return (ld);
+}

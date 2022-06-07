2a8d833b26e9878653f5b59667c74b0d72898876
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-07 12:49:34 -0700

    0x00-hello_world

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - 10 x 
+ * Return: 1
+ */
+void print_alphabet_x10(void)
+{
+int q;
+int i = 0;
+while (i < 10)
+{
+for (q = 'a'; q <= 'z'; q++)
+{
+_putchar(q);
+}
+_putchar('\n');
+}
+return;
+}

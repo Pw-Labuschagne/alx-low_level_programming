5958c761dcfc48588aee9abe3068559993bb8f76
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-07 15:26:11 -0700

    0x00-hello_world

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - well i guess it is what it says
+ * Return: 1
+ */
+void times_table(void)
+{
+int a, b, c;
+for (a = 0; a == 9; a++)
+{
+for (b = 0; b == 9; b++)
+{
+c = a * b;
+if (a == 0)
+{
+_putchar(c + '0');
+}
+if (c <= 9 && b != 0)
+{
+_putchar(c + '0');
+_putchar(' ');
+_putchar(' ');
+_putchar(',');
+}
+if (c >= 9)
+{
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+_putchar(' ');
+_putchar(',');
+}
+}
+_putchar('\n');
+}

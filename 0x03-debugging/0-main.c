fae02072be140f2a2a30bff6dea738b398b04bc2
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-07 22:46:30 -0700

    0x00-hello_world

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - test function that prints if integer is positive or negative
  * Return: 0
@@ -8,6 +7,6 @@ int main(void)
 {
 int i;
 i = 98;
-void positive_or_negative(int i);
+printf(positive_or_negative(i));
 return (0);
 }

6415f6ab4c008bc2086005f52e630b7e4a0a0b05
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-08 12:01:20 -0700

    0x00-hello_world

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - its something
+ * Return: 0
+ */
+int main(void);
+{
+int i, a, b, sum;
+a = 1;
+b = 2;
+sum = a + b
+printf("1, 2, \n");
+for (i = 4; i < 50; i++)
+{
+a = b;
+b = sum;
+sum = a + b;
+printf("%d, ", sum);
+}
+return;
+}

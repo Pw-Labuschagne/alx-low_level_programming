dd52d0ddfb7f497fd417155f5b6454b7dd45057e
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-08 11:46:28 -0700

    0x00-hello_world

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 int i, sum;
-for (i = 0; 1 < 1024; i++)
+for (i = 0; i < 1024; i++)
 {
 if ((i % 3) == 0 || ((i % 5) == 0))
 {
@@ -14,6 +14,5 @@ sum = sum + i;
 }
 }
 printf("%d\n", sum);
-return(0);
+return (0);
 }
-

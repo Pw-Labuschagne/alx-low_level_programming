c51ac341cf08c7117ec98652749bbca031dfd9ba
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-09 09:45:35 -0700

    0x00-hello_world

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,11 @@ if (a > b && a > c)
 {
 largest = a;
 }
-if (b > a && b > c)
+else if (b > a && b > c)
 {
 largest = b;
 }
-else if (c > a && c > b)
-{
+else
 largest = c;
-}
-return (largest);
+}return (largest);
 }

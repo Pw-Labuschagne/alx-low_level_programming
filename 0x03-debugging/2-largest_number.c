259a8be4fd0039716c709ff4027a56db7e169ea2
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-09 09:42:07 -0700

    0x00-hello_world

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * latgest_number - returns the largest of 3 numbers
+ * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
  * Return: Largest number
  */
 
-int larger_number(int a, int b, int c)
+int largest_number(int a, int b, int c)
 {
 int largest;
 

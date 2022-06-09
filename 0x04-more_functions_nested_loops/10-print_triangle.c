9d90eee343ea6aeb7251839a50e705e17d390f61
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-09 05:25:52 -0700

    0x00-hello_world

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - Well it prints a triangle
+ * @size: once again counter for size
+ * Return: 1
+ */
+void print_triangle(int size)
+{
+return;
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,9 @@
+#include <stfio.h>
+/**
+ * main - The best one so far
+ * Return: 0
+ */
+int main(void)
+{
+return (0);
+}

1776dd1148f02bcf1075bb3e2ee71c88d01f3035
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-09 13:26:54 -0700

    0x00-hello_world

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,5 +7,27 @@
  */
 void print_triangle(int size)
 {
-return;
+int x, y;
+if (size > 0)
+{
+for (x = 1; x <= size; x++)
+{
+for (y = 1; y <= size; y++)
+{
+if (y <= size - x)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }

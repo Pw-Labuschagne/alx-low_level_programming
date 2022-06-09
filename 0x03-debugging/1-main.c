94762b7076941eae4cbbe320580c5d862c23bbe8
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-09 03:20:29 -0700

    0x00-hello_world

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+#include <sdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+/**while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
+printf("infinite loop avoided \\o/\n");
+return (0);
+}
+

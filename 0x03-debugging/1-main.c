b280bc9d7db749f0c0091b04aaecd674fd89e7d7
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-09 03:25:56 -0700

    0x00-hello_world

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,12 +7,11 @@ int main(void)
 {
 int i;
 printf("Infinite loop incoming :(\n");
-/**while (i < 10)
+/* while (i < 10)
  * {
  * putchar(i);
  * }
  */
-printf("infinite loop avoided \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
-

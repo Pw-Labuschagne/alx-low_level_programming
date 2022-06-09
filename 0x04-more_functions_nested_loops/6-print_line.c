a6cecc6857ddf8b3854c5354285da532154a123a
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-09 11:54:42 -0700

    0x00-hello_world

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,15 +7,15 @@
 void print_line(int n)
 {
 int c;
-if (n > 0)
+if (n <= 0)
+{
+_putchar('\n');
+}
+else if (n > 0)
 {
 for (c = 0; c < n; c++)
 {
 _putchar('_');
 }
-}
-else
-{
-_putchar('\n');
 }
 }

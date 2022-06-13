2d010b4ddddb7b4f8133f6f8a16c6b9dabff9333
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-13 13:23:04 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rev_string - Reversed string 
+ * @s: placeholder of string char
+ * Return: 1
+ */
+void rev_string(char *s)
+{
+int x;
+for (x = 0; s[x] != '\0'; x++)
+{
+}
+while (x >= 0)
+{
+_putchar(s[x]);
+}
+return;
+}

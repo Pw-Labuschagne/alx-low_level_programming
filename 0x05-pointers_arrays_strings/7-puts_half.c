831c4f6245c9825db1db371de73d8a53c06bab21
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-13 13:59:18 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - prints half of string
+ * @str: The string
+ * Return: 1
+ */
+void puts_half(char *str)
+{
+int i;
+int c;
+for (i = 0; str[i] != '\0'; i++)
+{
+}
+if ((i % 2) = 0)
+{
+for (c = i; c >= (i / 2); c--)
+{
+_putchar(str[c]);
+}
+}
+else if ((i % 2) != 0)
+{
+c = ((i -1) / 2)
+_putchar(str[c]);
+}
+return;
+}

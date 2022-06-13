9601e06dc27807d5b5144b3776286cb97a038a69
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-13 14:12:57 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,20 +8,24 @@ void puts_half(char *str)
 {
 int i;
 int c;
+int x;
 for (i = 0; str[i] != '\0'; i++)
 {
 }
-if ((i % 2) == 0)
+if (((i % 2) == 0) && (i != 0))
 {
+i = i - 1;
 for (c = (i / 2); c <= i ; c++)
 {
 _putchar(str[c]);
 }
 }
-else if ((i % 2) != 0)
+else if (((i % 2) != 0) && (i != 0));
 {
 c = ((i -1) / 2);
+for 
 _putchar(str[c]);
 }
+_putchar('\n');
 return;
 }

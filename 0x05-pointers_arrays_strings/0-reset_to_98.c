26ebc0c8559177cec3fd2616ff925d21b15e55d0
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-13 11:36:09 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - It takes a pointer and updates its
+ * @*n:its the placeholder for the int
+ * Return: 1
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+return;
+}

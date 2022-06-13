fe603dfb306994c72625990eeedf22e4b6390c82
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-13 11:51:25 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - Swaps two integers
+ * @*a: Place holder int 1
+ * @*b: Place holder int 2
+ * Return: 1
+ */
+void swap_int(int *a, int* b)
+{
+int a, b;
+a = *a;
+b = *b;
+}

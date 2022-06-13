63f0a013f59165ac850772ffc3ce0f0a29d2dbef
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-13 14:55:38 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - Pointing copied string to dest
+ * @dest: Propably destination
+ * @src: Probably source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int c;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+dest[i] = '\0';
+return (dest)
+}
+}

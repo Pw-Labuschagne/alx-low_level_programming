9da112f80198e89bc07fc6bbc1f20020cc6efd1a
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-16 10:05:07 -0700

    0x06-More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,23 @@
 #include "main.h"
 /**
- *
+ * leet - encodes to 1337
+ * @c: Character to read
+ * Return: 0
  */
-char *leet(char *)
+char *leet(char *c)
 {
-return ();
+char a[] = "aAeEoOtTlL";
+char n[] = "4433007711";
+int i = 0, z;
+for ( ; c[i] != '\0'; i++)
+{
+for (z = 0; z < 9; z++)
+{
+if (a[z] == c[i])
+{
+c[i] = n[z];
+}
+}
+}
+return (c);
 }

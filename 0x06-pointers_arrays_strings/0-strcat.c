867373dc385141571661d2ebddc4af012c4da493
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-16 04:05:02 -0700

    0x06-More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,25 +7,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-char cat1[] = "src";
-char cat2[] = "dest";
-char catd[100];
-
 int a = 0, b = 0;
 
-while (cat1[a] != '\0')
+while (dest[a] != '\0')
 {
-catd[b] = cat1[a];
+a++;
+}
+b = 0;
+while (src[b] != '\0')
+{
+dest[a] = src[b];
 a++;
 b++;
 }
-a = 0;
-while (cat2[a] != '\0')
-{
-catd[b] = cat2[a];
-a++;
-b++;
-}
-return (catd);
+return (dest);
 }
 

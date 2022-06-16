1e261806a1731167f56d240c58636a4038c5897b
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-16 08:24:07 -0700

    0x06-More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,18 @@
 #include "main.h"
 /**
- *
+ * cap_string - Capitalizes all words of a string
+ * @str: String
+ * Return: 0
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
+int i;
+while (str[i] != '\0')
+{
+if (i = 0; (str[i] >= 32) && (str[i] <= 47); i++)
+{
+str[i++] = str[i++] - 32;
+}
+}
 return ();
 }

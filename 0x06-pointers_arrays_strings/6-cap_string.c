720c6258e42d8788ba16c62502f527bef53eb179
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-16 08:49:07 -0700

    0x06-More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,12 +9,18 @@ char *cap_string(char *str)
 int i = 0;
 while (str[i] != '\0')
 {
-if (str[i] >= 32 && str[i] <= 47)
+if (str[i] <= 122 && str[i] >= 97)
 {
-i++;
 str[i] = str[i] - 32;
 }
+if (str[i] == 32 || str[i] == '\t' || str[i] == '\n' || str[i] == 44 || str[i] == 59 || str[i] == 46 || str[i] == 33 || str[i] == 63 || str[i] == 34 || str[i] == 40 || str[i] == 41 || str[i] == 123 || str[i] == 125)
+{
+if (str[i + 1] <= 122 && str[i + 1] >= 97)
+{
+str[ i + 1] = str[i + 1] - 32;
+}
+}
 i++;
 }
-return (0);
+return (str);
 }

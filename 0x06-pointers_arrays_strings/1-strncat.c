950bf5dd57146e6c67d927174d3625ebd0785674
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-16 04:19:41 -0700

    0x06-More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,23 @@
 #include "main.h"
 /**
- *
+ * _strncat - concatenates two strings
+ * @dest: input string
+ * @src: input string
+ * @n: byte determinator
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-return ();
+int a = 0, b = 0;
+while (dest[a] != '\0')
+{
+a++
+}
+while (src[b] != '\0' && b < n)
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+return (dest);
 }

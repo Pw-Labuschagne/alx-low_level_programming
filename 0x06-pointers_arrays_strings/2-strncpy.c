d91fc22ca2544fb375ac1651c858fa202eb6fa57
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-16 04:43:03 -0700

    0x06-More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
+int i = 0, a = 0;
 while (*src != '\0' && i < n)
 {
-*dest = *src;
-*dest++;
-*src++;
+dest[a] = src[a];
+a++;
 i++;
 }
 *dest = '\0';

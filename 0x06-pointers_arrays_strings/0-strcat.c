736748f73557cd0f191638d16410ccdbd60d64fa
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-16 03:52:29 -0700

    0x06-More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,31 @@
 #include "main.h"
 /**
  * strcat - Concatenate two strings
- * Return
+ * @src: Source code
+ * @dest: Destination code
+ * Return: Concated string
  */
 char *strcat(char *dest, char *src)
 {
-char cat[] = "*src" "*dest";
+char cat1[] = "*src";
+char cat2[] = "*dest";
+char catd[];
+
+int a = 0, b = 0;
+
+while (cat1[a] != '\0')
+{
+catd[b] = cat1[a];
+a++;
+b++;
+}
+a = 0;
+while (cat2[a] != '\0')
+{
+catd[b] = cat[a];
+a++;
+b++;
+}
 return (cat);
 }
 

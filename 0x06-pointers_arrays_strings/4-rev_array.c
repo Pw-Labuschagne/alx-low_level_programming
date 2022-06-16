98f399f9841a7ad7372a3d745346cea1730b3c02
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-16 07:53:37 -0700

    0x06-More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,23 +3,18 @@
  * reverse_array - Reverses contents of array of integers
  * @a: the array
  * @n: number of elements to swap
+ * Return: 0
  */
 void reverse_array(int *a, int n)
 {
-int i, len, start, temp;
-start = 0;
-temp = 0;
-for (i = 0; a[i] != '\0'; i++)
+int i, temp;
+i = 0;
+n--;
+while (i <= n)
 {
+temp = a[i];
+a[i++] = a[n];
+a[n--] = temp;
 }
-len = i;
-for (i = 0; i < n; i++)
-{
-temp = a[start];
-a[start] = a[len];
-a[len] = temp;
-start++;
-len--;
-}
-return ();
+return;
 }

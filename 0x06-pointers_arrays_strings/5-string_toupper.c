27c02df09987057ac7818419cf80539912163938
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-16 08:04:01 -0700

    0x06-More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * string_toupper - Changes string to uppercase letters
- * @l: Character to check 
+ * @l: Character to check
  * Return: 0
  */
 char *string_toupper(char *l)
@@ -9,7 +9,7 @@ char *string_toupper(char *l)
 int c = 0;
 if (l[c] != '\0')
 {
-while ((l[c]>= 97) || (l[c] <= 122))
+while ((l[c] >= 97) || (l[c] <= 122))
 {
 l[c] = l[c] - 32;
 }

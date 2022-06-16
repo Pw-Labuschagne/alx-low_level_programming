8c5d4b22181c83c594bba9589d0c5ccd14448fda
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-16 10:12:30 -0700

    0x06-More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@ char *leet(char *c)
 char a[] = "aAeEoOtTlL";
 char n[] = "4433007711";
 int i = 0, z = 0;
-while (c[i] != '\0'; i++)
+while (c[i] != '\0')
 {
 while (z < 9)
 {
@@ -17,8 +17,9 @@ if (a[z] == c[i])
 {
 c[i] = n[z];
 }
-}
 z++;
+}
+i++;
 }
 return (c);
 }

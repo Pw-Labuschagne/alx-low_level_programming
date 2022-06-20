c0005be99b1ed0b2f834364822005059da8cbd9b
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-20 13:18:07 -0700

    0x07-Even more pointers, arrays, strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - Fills memory with a constant byte
+ * @s: Starting address of memory to be filled
+ * @b: Value to be filled
+ * @n: number of bytes to be filled starting from ptr
+ * Return: 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+for (i = 0; i < n ; i++)
+{
+*s = b;
+}
+return (0);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,8 @@
+0.Fills memory with constant type
+1.Copies memory data
+2.Locates a char in string
+3.Return lenght of prefix substring
+4.Searches for any set of bytes
+5.Locates a substring
+6.Prints chessboard
+7.Prints sum of two diagonal square matrix of integers

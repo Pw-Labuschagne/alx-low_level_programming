e1083df0779e3f33161f0d623a4e905e1cdef9db
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-21 12:29:55 -0700

    0x08-recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,6 +4,19 @@
  * @n: Number to factorial
  * Return: Factorial of number n
  */
-int factorial(int n);
+int factorial(int n)
 {
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{
+return (1);
+}
+if (n != 0)
+{
+return (n * factorial(n-1)); 
+}
+return (0);
 }

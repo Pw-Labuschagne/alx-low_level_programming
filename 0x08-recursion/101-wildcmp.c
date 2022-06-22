8f80a0bb252a561096284dec2f1aeb30216a4e12
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-22 10:46:51 -0700

    0x08-recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * wildcmp - wild man
+ * @s1: first string value
+ * @s2: second string value
+ * Return: 0 or 1
+ */
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 != *s)
+{
+return (0);
+}
+}

17009754327b860845d41c2868a4780b6428ee35
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-26 01:21:41 -0700

    0x0A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Returns number of arguments passed into it
+ * @argc: counter of arguments
+ * @argv: the array of where the input is stored
+ * Return: 0
+ */
+int nargs(int argc, __attribute__((unused)) char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
+int main(__attribute__((unused)) int argc, char **argv);
+int nargs(int argc, __attribute__((unused)) char *argv[]);
 
 #endif /* MAIN_H */

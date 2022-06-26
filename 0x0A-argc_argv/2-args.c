c57c9570b5209f8f7bdb0712cdcd414cc5804690
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-26 01:40:30 -0700

    0x0A

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Returns all arguments received
+ * @argc: number of arguments
+ * @argv: the array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+}
+return (0);
+}

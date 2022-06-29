335b70b26eca910b358d960cb3155515a39e0058
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-29 11:08:27 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,8 +8,17 @@
 char *_strdup(char *str)
 {
 int i = 0, c;
-char *s = malloc(*str);
+char *s;
 if (str == 0)
+{
+return (0);
+}
+while (str[i] != '\0')
+{
+i++
+}
+s = malloc(sizeof (int) * i);
+if (s == 0)
 {
 return (0);
 }

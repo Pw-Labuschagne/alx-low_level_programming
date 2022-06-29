f596282681cf2705788d66ef98c4ebd1ea9ca414
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-06-29 05:01:13 -0700

    0x0A

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,16 +9,23 @@
 int main(int argc, char *argv[])
 {
 int i, sum = 0;
+if (argc < 1)
+{
+printf("0\n");
+}
+else
+{
 for (i = 0; i < argc; i++)
 {
-if (*argv[i] >= 12)
+if ((*argv[i] < 0) || (*argv[i] >= 'a' && *argv[i] <= 'z') || (*argv[i] >= 'A' &&*argv[i] <= 'Z'))
 {
 printf("Error\n");
 return (1);
 }
 else
 {
-sum = sum + *argv[i];
+sum = sum + (atoi(argv[i]));
+}
 }
 }
 printf("%d\n", sum);

de8cec08d98ba46c623c6f884c9e29b32a035000
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-07-01 12:35:36 -0700

    0x0C-More malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * mallock_checked - allocates memory using the string
+ * @b: size needed
+ * Return: 0
+ */
 void *malloc_checked(unsigned int b)
 {
-b = 2;
+void *s;
+s m= malloc(b);
+if (s == 0)
+{
+exit(98);
+}
+return (s);
 }

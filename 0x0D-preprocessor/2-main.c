4faadba2cd2db5309ed67768f2ac715d0e17c6f0
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-07-04 05:12:03 -0700

    0x0D

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - returns name of file it was compiled from
+ * @argv: 0 is the executable file name
+ * @argc: not being used
+ * Return: 0
+ */
+int main(char argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}

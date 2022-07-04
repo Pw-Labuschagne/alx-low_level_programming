8952ff61f21a89071d2f868adb8b9c0147c3e955
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-07-04 14:25:45 -0700

    0x0E

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,14 +17,7 @@ printf("Name: %s\n", d->name);
 {
 	printf("Name: (nill)\n");
 }
-if (d->age != 0)
-{
 printf("Age: %.1f\n", d->age);
-}
-	else
-{
-	printf("Age: (nill)\n");
-}
 if (d->owner != NULL)
 {
 printf("Owner: %s\n", d->owner);

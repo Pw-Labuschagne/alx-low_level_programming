eb2c69cbc3e1d29aa15b263157df0d6e0de6a278
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-07-04 13:49:31 -0700

    0x0E

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <sstdio.h>
+#include "dog.h"
+/**
+ * init_dog - initialize new variable struct dog
+ * @d: placeholder for address
+ * @name: name 
+ * @age: age
+ * @owner: owner of dog
+ * Return: 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
+}

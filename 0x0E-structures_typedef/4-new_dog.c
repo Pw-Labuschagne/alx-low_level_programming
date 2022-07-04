1b5fdd1ba451ca9fb78348a6efffecf81c8c26c9
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-07-04 15:10:08 -0700

    0x0E

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+/**
+ * new_dog - creates a new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: 0
+ */
+struct dog_cp
+{
+char *N;
+float A;
+char *O;
+}
+struct dog_cp new_dog;
+{
+dog_cp = malloc(sizeof(struct dog_cp));
+N = new_dog.name;
+A = new_dog.age
+O = new_dog.owner;
+}
+
+dog_t *new_dog(char *name, float age, char* owner)
+{
+new_dog->name = name;
+new_dog->age = age;
+new_dog->owner = owner;
+}

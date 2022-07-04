91a01516d5a2baf9592400870b1c4ea05f5b1fd1
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-07-04 09:13:27 -0700

    0x0D

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,17 +2,10 @@
 #include <stdlib.h>
 /**
  * main - returns name of file it was compiled from
- * @argv: 0 is the executable file name
- * @argc: not being used
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-int i;
-while (i < argc)
-{
-printf("%s\n", argv[i]);
-i++;
-}
+printf("%s\n", __FILE__);
 return (0);
 }

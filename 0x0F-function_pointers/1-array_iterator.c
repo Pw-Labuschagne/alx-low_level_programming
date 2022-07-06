21abfc3d2207aa5a065152d9c2d8d658ac0dade9
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-07-06 11:41:31 -0700

    Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 long unsigned int i;
-	if (array != NULL && size != 0)
-	{
-		for (i = 0; i < size; i++)
-			{
-				action(array[i]);
-			}
-	}
+if (array != NULL && size != 0)
+{
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
+_putchar('\n');
 }

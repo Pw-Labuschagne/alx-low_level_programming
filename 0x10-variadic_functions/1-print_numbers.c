df4e669102fda77e72bccbebb5e76811f906b853
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-07-08 13:04:23 -0700

    git_0x10

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,7 +8,7 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list list
+	va_list list;
 		unsigned int a;
 	if (n == 1)
 	{
@@ -21,15 +21,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 	for (a = 0; a < n - 1; a++)
 	{
-		if (seperator == NULL)
+		if (separator == NULL)
 		{
 			printf("%i", va_arg(list, int));
 		}
 		else
 		{
-			printf("%i%s", va_arg(list, int), seperator);
+			printf("%i%s", va_arg(list, int), separator);
 		}
-		if (i == n - 1)
+		if (a == n - 1)
 		{
 			printf("%i\n", va_arg(list, int));
 		}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,7 @@
 #define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
-void print_number(const char *seperator, const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
 void prints_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 

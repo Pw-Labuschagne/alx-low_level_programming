8028d92f6cee8ab01739b074c307878d2eb009f7
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-07-08 13:09:33 -0700

    git_0x10

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,33 +9,32 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
-		unsigned int a;
 
-		va_start(list, n);
+	unsigned int a;
+
+	va_start(list, n);
 
 	if (n == 1)
 	{
 		printf("%i\n", va_arg(list, int));
 	}
+
 	if (n == 0)
 	{
 		printf("\n");
 		return;
 	}
+
 	for (a = 0; a < n - 1; a++)
 	{
 		if (separator == NULL)
-		{
-			printf("%i", va_arg(list, int));
-		}
+			printf("%i", va_arg(list, int))
 		else
-		{
 			printf("%i%s", va_arg(list, int), separator);
 		}
 		if (a == n - 1)
-		{
 			printf("%i\n", va_arg(list, int));
-		}
+
 		va_end(list);
-	}
+}
 }

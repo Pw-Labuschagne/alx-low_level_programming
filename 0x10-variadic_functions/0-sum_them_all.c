2efdc4b1911f982d7fb9fd1f0e83d8a15c9770fc
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-07-08 12:35:16 -0700

    git_0x10

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - adds all parameters 
+ * @n: Ubsigned int for arguments
+ * @...: variable arguments
+ * @Return: 0 for n == 0 and int as suc
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list sum;
+
+unsigned int c;
+int all = 0;
+
+if (n == 0)
+{
+return (0);
+}
+va_start(sum, n);
+
+for (c = 0; c < n; c++)
+{
+all += va_arg(sum, int);
+}
+va_end(sum);
+
+return (all}
+}
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,4 @@
+0.Returns sum of all parameters
+1.Prints numbers 
+2.Prints string
+3.Prints aything

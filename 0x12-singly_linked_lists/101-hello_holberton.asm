4099228715e1095461a541389ea23afd0942beea
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-14 11:25:27 -0700

    0x12

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+; This was something new
+;Not really aware of the whole thing, but getting there
+section .text
+	global _start
+
+_start :
+	mov edx, len
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4 ;sys call sys_write
+	int 0x80 ;calls kernel
+
+	mov eax, 1 ;system call sys_exit
+
+section .data
+	msg db "Hello, Holberton", 10 ;allocates memory. 10 = newline or 0xa
+	len equ $ -msg ;length

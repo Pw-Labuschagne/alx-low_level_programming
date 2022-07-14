eabc4bbf4c41050fafc59231cce617415100a674
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-14 10:32:59 -0700

    0x12

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * add_node_end - Adds node to the end of list
+ * @head: Pointer to list 
+ * @str: string to be added
+ * Return: Address of new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+unsigned i = 0;
+list_t *ptr = malloc(sizeof(list_t));
+list_t *temp = malloc(sizeof(list_t));
+
+if (str == NULL || ptr == NULL)
+{
+	return (0);
+}
+
+while(str[i] != '\0')
+{
+	i++;
+}
+
+if (*head == NULL)
+{
+*head = ptr;
+ptr->len = i;
+ptr->next = NULL;
+ptr->str = strdup(str);
+
+return (ptr);
+}
+
+temp = *head;
+
+while (temp->next != NULL)
+{
+	temp = temp->next;
+}
+temp->next = ptr;
+ptr->str = strdup(str);
+ptr->next = NULL;
+ptr->len = i;
+
+return (ptr);
+}

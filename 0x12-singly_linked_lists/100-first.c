9850e13720217c239cca61a21ffbe3817c86f2a2
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-14 11:09:15 -0700

    0x12

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,11 @@
-#include <stio.h>
+#include <stdio.h>
 /**
  * startup - executes and prints string before main is executed.
  * Return: nothing
  */
-void startup (void) __attribute__ ((constructor))
+void startup (void) __attribute__ ((constructor));
+
+void startup (void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }

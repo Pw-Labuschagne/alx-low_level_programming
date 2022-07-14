dbbbccf68ff11b899afeb936a92dbd4f3b5297ac
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-14 10:45:33 -0700

    0x12

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,17 @@
 #include "lists.h"
 /**
  * list_len - Returns number of elements in a linked list list_t
- * @list_t: linked list provided
  * @h: pointer to list_t string
  * Return: number of elements int list_t
  */
 size_t list_len(const list_t *h)
 {
 int i = 0;
+
+if (h == NULL)
+{
+	return (0);
+}
 
 	while (h != NULL)
 	{

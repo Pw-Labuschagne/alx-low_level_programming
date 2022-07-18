e5d07730e1141c734e17c70ae067ba5ca0c4770a
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-18 10:45:32 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,9 +9,9 @@ void free_listint(listint_t *head)
 listint_t *frees;
 
 while (head != NULL)
-	{
-		frees = head;
-		head = head->next;
-		free(frees);
-	}
+{
+frees = head;
+head = head->next;
+free(frees);
+}
 }

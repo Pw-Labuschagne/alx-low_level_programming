326c9dbd22462abc6eff4ba5d0e120df441d7c83
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-18 10:48:46 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,6 +4,15 @@
  * @head: Pointer to list
  * Return: Head node address
  */
-void free_listint2(list_t **head)
+void free_listint2(listint_t **head)
 {
+	listint_t *frees;
+
+	while (head != NULL)
+	{
+		frees = head;
+		head = head->next;
+		free(frees);
+	}
+	head = NULL;
 }

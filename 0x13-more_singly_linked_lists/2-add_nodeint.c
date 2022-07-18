7c09f49b738131d47381ebaad481464f9f804d8c
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-18 10:15:21 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,16 +9,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 
 listint_t *ptr = malloc(sizeof(listint_t));
-
-if (head == NULL)
-{
-	return (NULL);
-}
-if (n == '\0')
-{
-ptr->n = '\0';
-return (NULL);
-}
 ptr->n = n;
 ptr->next = NULL;
 

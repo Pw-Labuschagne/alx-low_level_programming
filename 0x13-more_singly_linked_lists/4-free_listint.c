c4b91eaf6d8642228b88aa9b7a3e7b6336b4bde4
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-18 10:33:16 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,8 +2,14 @@
 /**
  * free_listint - Frees a list
  * @head: Pointer to list
- * Return: nothgin
+ * Return: nothing
  */
 void free_listint(list_t **head)
 {
+	listint_t *frees;
+	for (frees = head; frees != NULL; frees = free->next)
+	{
+		free(frees->n);
+		free(frees);
+	}
 }

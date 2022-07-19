877aedf2e41cb6b28817ef14b0bd2139b232f527
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-19 09:53:19 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
-/**get_node_at_index - Gets a node at given index
+/**
+ * get_nodeint_at_index - Gets a node at given index
  * @head: pointer to list
  * @index: Place to get the node
  * Return: address of node
@@ -8,14 +9,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	while (i < index)
-		{
-			head = head->next;
-			i++;
-		}
-	if (head == NULL)
-	{
-		return (0);
-	}
+while (i < index)
+{
+	head = head->next;
+	i++;
+}
+if (head == NULL)
+{
+	return (0);
+}
 return (head);
 }

108fb8a1deb381b4a9f50d11fa0b50fb828c5d86
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-19 11:12:32 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,10 +2,33 @@
 /**
  * insert_nodeint_at_index - Inserts a node at index
  * @head: Pointer to list
- * @ind: Point where to be inserted
+ * @idx: Point where to be inserted
  * @n: integer to insert
  * Return: address of new node or NULL
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int ind, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+
+listint_t *move = *head;
+listint_t *new = malloc(sizeof(listint_t));
+
+new->n = n;
+new->next = NULL;
+
+if (new == NULL || *head == NULL)
+{
+	return (0);
 }
+
+idx--;
+while (idx != 1)
+{
+	move = move->next;
+	idx--;
+}
+new->next = move->next;
+move->next = new;
+
+return (new);
+}
+

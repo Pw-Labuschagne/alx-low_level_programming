3e1cfadcce0a02435ab758334072727e240be4aa
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-21 11:52:38 -0700

    0x14-bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,32 @@
 #include "main.h"
 /**
- * binary_to_int - Converts binary to unsigned int
+ * binary_to_uint - Converts binary to unsigned int
  * @b: Number passed to be converted(string)
  * Return: Converted number,or 0 if more than one chars not base2 | b = o
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
+	int i;
 	unsigned int num = 0;
-	char str[] = b;
 
-	while (str[i] != '\0')
+	if (b == NULL)
 	{
-		if (str[i] != '1' || str[1] != '0' || b == NULL)
+		return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '1' && b[i] != '0')
 		{
 			return (0);
 		}
-
-		i++;
-	}
-
-i = 0;
-num = (0 * 2) + atoi(str[i]);
-
-	while (str[i] != '\0')
+	if (b[i] == '1')
 	{
-		num += (num * 2) + atoi(str[i]);
-		i++;
+		num = (num * 2) + 1;
 	}
+	else if (b[i] == '0')
+	{
+		num = (num * 2) + 0;
+	}
+}
 return (num);
 }

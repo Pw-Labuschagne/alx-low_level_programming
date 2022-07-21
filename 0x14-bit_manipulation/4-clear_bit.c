ef42db85e8832984f48b8ddd87abd27e67bb42c4
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-21 13:50:54 -0700

    0x14-bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,12 +5,12 @@
  * @index: Given index to change bit
  * Return: 1 if successful and -1 if error
  */
-int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n ^= 0 << index;
+	*n &= ~(1 << index);
 
 	return (1);
 }

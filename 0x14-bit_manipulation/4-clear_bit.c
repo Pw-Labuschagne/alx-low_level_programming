3182945cd4944c685501c71386f8f372c514b81d
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-21 13:45:25 -0700

    0x14-bit manipulation

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * clear_bit - Sets bit to 0 at given index
+ * @n: Integer to change bit
+ * @index: Given index to change bit
+ * Return: 1 if successful and -1 if error
+ */
+int clear_bit(unsigned long int *n, unsigned int index);
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n ^= 0 << index;
+
+	return (1);
+}

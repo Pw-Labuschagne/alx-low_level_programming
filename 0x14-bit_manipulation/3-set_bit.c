eb774952452e348eaba61d648920a62669033074
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-21 13:20:27 -0700

    0x14-bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,9 +8,9 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned x = 1;
-	unsigned long int bit = n;
+	unsigned long int bit = *n;
 
-	if (index => (sizeof(unsigned int) * 8 ))
+	if (index <= (sizeof(unsigned int) * 8 ))
 	{
 		return (-1);
 	}
@@ -19,7 +19,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	bit = x << index;
 
-	bit = bit | n;
+	bit = bit | *n;
 
 	return (1);
 

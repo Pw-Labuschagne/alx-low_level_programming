7df3da9edffc96c5f9afee8f2842822b82ba666f
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-07-21 12:40:57 -0700

    0x14-bit manipulation

---
+++ 0x14-bit_manipulation/2-git_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - Returns value of bit at given index
+ * @n: Integer to determine bit
+ * @index: The given point where bit should return
+ * Return: The bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int num;
+
+	num = (n >> index) & 1;
+	
+
+	return (x);
+
+}

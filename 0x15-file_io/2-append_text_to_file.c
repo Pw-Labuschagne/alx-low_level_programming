fe1c99099ec26a6437880f186a9c0d5d7a736b58
Pw-Labuschagne <Pw.labuschagne@yahoo.com>
2022-07-28 06:05:30 -0400

    0x15

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * append_text_to_file - Appends text to a file
+ * @filename: File to append  
+ * @text_content: Content to append to file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+	int fd, append_me;
+
+	if (filename == NULL)
+		return (-1)
+
+	if (text_content == NULL && filename)
+		return (1);
+
+	fd = open(filename, O_APPEND);
+
+	if (fd < 0)
+		return (-1);
+
+	append_me =  write(fd, text_content);
+
+	if (append_me < 0)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+
+	
+
+
+}

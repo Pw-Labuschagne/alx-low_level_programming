cfa8c0b0bfd9beee57f3ea944780b2b4537968f6
Pw-Labuschange <Pw.labuschagne@yahoo.com>
2022-08-15 14:45:08 -0400

    0x00-hello_world

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,9 +9,13 @@ unsigned long int i, a, b, sum;
 a = 0;
 b = 1;
 sum = a + b;
-for (i = 1; i < 51; i++)
+printf("%lu", sum);
+a = b;
+b = sum;
+sum = a + b;
+for (i = 1; i < 50; i++)
 {
-printf("%lu, ", sum);
+printf(", %lu", sum);
 a = b;
 b = sum;
 sum = a + b;

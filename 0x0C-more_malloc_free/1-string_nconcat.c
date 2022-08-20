4e95df072f2ed988529e980fb5e5ca50b94fa4e1
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-20 14:30:36 +0200

    0x0C-More malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,65 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-/**
- * string_nconcat - Concatenates two stings
- * @s1: String one
- * @s2: String two
- * @n: counter?
- * Return: 0
- */
-int _sl(char *i) /** not allowdto use standard functions??*/
-{
-	int a;
 
-	for (a = 0; i[a] != '\0'; a++)
-		;
-	return (a);
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: char pointer for string
+ * Return: int for length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (_strlen_recursion(s + 1) + 1);
+
+	return (0);
 }
+
+/**
+ * string_nconcat - function that concatenates two strings with malloc'd memory
+ * @s1: Char pointer for string 1
+ * @s2: Char pointer for string 2
+ * @n: Unsigned int for memory size to be allocated
+ * Return: Char pointer of concatenated string pointed to a newly allocated
+ * space in memory, return NULL if function fails
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *c;
-	unsigned int z, x, y;
+	char *a;
+	unsigned int i, j, l1, l2, size;
+
+	if (s1 == NULL)
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 == "";
-	if ( s1 == NULL)
-		s1 == NULL;
+		s2 = "";
 
-	z = _sl(s1);
-	x = _sl(s2);
+	l1 = _strlen_recursion(s1);
+	l2 = _strlen_recursion(s2);
 
-	if (n < 1)
-		x = n;
-	c = malloc(z + 1 + 1);
-if (c == NULL)
-{
-	return (NULL);
-}
-for (y = 0; y < z; y++)
-{
-	*(c + 1) = *(s1 + y);
-}
-for (y = 0; y < x; y++)
-{
-	*(c + (y + z)) = *(s2 + y);
-	*(c + (y + x)) = '\0';
-}
-return (s);
+	if (n >= l2)
+		size = l1 + l2 + 1;
+
+	if (n < l2)
+		size = l1 + n + 1;
+
+	a = malloc(sizeof(char) * size);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		a[i] = s1[i];
+
+	if (n >= l2)
+		for (j = 0; s2[j] != '\0'; j++)
+			a[i + j] = s2[j];
+
+	if (n < l2)
+		for (j = 0; j < n; j++)
+			a[i + j] = s2[j];
+
+	a[i + j] = '\0';
+	return (a);
 }

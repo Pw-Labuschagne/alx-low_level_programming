3fad17dcb150e57b8fe31c7231183508a8169a5f
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-20 14:02:22 +0200

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,18 +10,17 @@
 int _atoi(char *s)
 {
 	int a, b, c, srt, i, num;
+	unsigned int n;
+
 	srt = 0;
 	num = 0;
 	i = 0;
 	c = 0;
-	unsigned int n;
-	
-	while(*(s + c) < 48 || *(s + c) > 57)
+
+	while (*(s + c) < 48 || *(s + c) > 57)
 	{
 		if (*(s + c) == '-')
-		{
 			i++;
-		}
 		if (*(s + c) == '\0')
 		{
 			b = 0;
@@ -30,26 +29,19 @@ int _atoi(char *s)
 		}
 		srt++;
 		c++;
-
 		if (c + 1 < c)
 			return (0);
-	}
-	while (*(s + c) >= 48 && *(s +c) <= 57)
+	while (*(s + c) >= 48 && *(s + c) <= 57)
 	{
 		c++;
 		++num;
 	}
-
 	n = 0;
-
 	for (a = srt; a < (num + srt); a++)
 	{
 		n = n * 10 + s[a] - '0';
-
 		if (i % 2 != 0)
-		{
 			n = -n;
-		}
 		b = n;
 		return (b);
 	}

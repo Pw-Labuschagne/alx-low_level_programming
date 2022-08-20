0ff993bf11e6245ea6d657282379b832d04a4328
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-08-20 14:32:15 +0200

    0x0C-More malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,4 +9,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+
+	void *a;
+
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	a = malloc(nmemb * size);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		*((unsigned char *) a + i) = 0;
+
+	return (a);
 }

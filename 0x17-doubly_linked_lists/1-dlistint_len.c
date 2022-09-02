ec1611e512eb6fc04e38b1165c118592502f0a09
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-02 03:38:45 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,29 @@
 #include "lists.h"
+
+/**
+ * dlistint_len - get the length of nodes in a double linked list
+ * @h: Head of list
+ * Return: Number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+	int prt = 0;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h-> prev != NULL && prt == 0)
+		{
+			h = h->prev;
+			continue;
+		}
+		else if (h-prev == NULL)
+		{
+			prt = 1;
+			i = 0;
+		}
+		h = h->next;
+	}
+	return (i);
+}

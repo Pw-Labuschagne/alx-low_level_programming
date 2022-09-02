f60e0341e4b99fd066f2d82f7ec689be55e2a82e
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2022-09-02 03:56:53 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,24 @@
 #include "lists.h"
+
+/**
+ * free_dlistint - function that frees a list
+ * @head: head of list
+ * Return: nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	tmp = head;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (tmp != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}

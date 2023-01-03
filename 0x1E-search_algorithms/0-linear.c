81da9af0f057c3423d5c574c5e1c66c351a0266c
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2023-01-03 17:47:44 +0200

    Searching Algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * linear_search - Linear search array for value
+ * array: Array to search
+ * size: Size of arrar(num elements)
+ * value: Value to search for
+ * Return: -1 if not found or array==NULL else value
+*/
+
 int linear_search(int *array, size_t size, int value)
 {
 	int num;
 	size_t c;
+	
+	if(array == NULL)
+		return(-1);
 
 	for(c = 0; c < size; c++)
 	{

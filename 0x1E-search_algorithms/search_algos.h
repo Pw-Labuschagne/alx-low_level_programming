6b051a46a745e8dbcf14ae673c6a855ab1aad3cc
Pw-Labuschagne <pw.labuschagne@yahoo.com>
2023-01-18 16:43:28 +0200

    Pushed this time

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,9 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARHC_ALGOS_H
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
